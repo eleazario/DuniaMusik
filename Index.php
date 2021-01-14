52cc1dc9d69b591d06006ea3167f49ee65cca2c9
Eleazar Ioanes Ekha Benrhio <62139706+eleazario@users.noreply.github.com>
2021-01-14 14:06:29 +0700

    Revert "Update Index.php"
    This reverts commit 53257a54702a390bfb32255c552e6c4a93cf66d0.

--- Index.php
+++ Index.php
@@ -12,7 +12,7 @@
 
       <!-- Home -->
           <div class="jumbotron jumbotron-fluid"><br>
-            <div class="">
+            <div class="container-fluid">
                 <?php
                   session_start();
                   if(isset($_SESSION['user'])){
@@ -85,66 +85,6 @@
           </div>
           
           <?php include_once 'script.html'?>
-          <script>
-                          $('#FormSearch').submit(function(e){
-                  $.ajax({
-                      url: 'https://accounts.spotify.com/api/token',
-                      type: 'post',
-                      dataType: 'json',
-                      headers: {
-                          'Authorization': 'Basic ODRjODM3Mjg3ZGZlNDcyZjllYTlhOGE5YTMxMTMyZTM6NzdmOGE4NDQ3YzlhNDE2NGFiYzUwMWViM2UxMzc2NTU=',
-                          'Content-Type': 'application/x-www-form-urlencoded'
-                      },
-                      data: {
-                          'grant_type': 'client_credentials'
-                      },        
-                      success:function(auth){
-                          $.ajax({
-                              url: 'https://api.spotify.com/v1/search',
-                              type: 'get',
-                              dataType: 'json',
-                              headers: {
-                                  'Authorization' : auth.token_type + ' ' + auth.access_token                    
-                              },
-                              data: {
-                                  'q': $('#FormSearch input').val(),
-                                  'type': 'track,artist',
-                                  'market': 'ID',
-                                  'limit': '10',
-                                  'offset': '5'
-                              },
-                              success:function(result){
-                                  var track = result.tracks.items;
-                                  console.log(track);
-                                  $('#Content').html('');
-
-                                  $('#Content').append('<div class="judul"><h1 class="font-weight-bold text-light">S E A R C H</h1><hr class="garis"></div><br><br><div class="search p-5"></br><ul id="UlItem" class="list-group" style="opacity: 100%;"></ul>');
-                                  $.each(track,function(i,data){
-                                      var artist = data.artists;
-                                      
-                                      albumRead = function(){
-                                          $.each(artist,function(n,index){
-                                              return(index.name);
-                                          });
-                                      }
-                                      $('#UlItem').append('<li class="list-group-item bg-secondary"><h5>'+ data.name +'</h5>'+ data.album.name +'<br><b>' +'</b></li>');
-                                  
-                                  })
-                              },
-                              error:function(result){
-                                  
-                                  alert(result.responseJSON.error.message)
-                              }
-                          })
-                      },
-                      error:function(auth){
-                          alert('auth error');
-                      }
-                  })
-                  e.preventDefault()
-              })
-
-          </script>
   </body>
 </body>
 </html>
